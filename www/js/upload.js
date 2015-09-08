6f8509144ab22b0b3e40760857d8532bd9ae4fbb
Xavier <xavierchen00@gmail.com>
2015-09-08 14:47:11 -0400

    Reverted

--- www/js/upload.js
+++ www/js/upload.js
@@ -1,4 +1,5 @@
 
+//
 var pictureSource; // picture source
 var destinationType; // sets the format of returned value
 var imagedata = "";
@@ -17,23 +18,81 @@ function onDeviceReady() {
 
 // Called when a photo is successfully retrieved
 //
+function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64-encoded image data
+      // console.log(imageData);
 
-function getPhoto(source) {
-    // Retrieve image file location from specified source
-    navigator.camera.getPicture(onPhotoURISuccess, onFail, {quality:50, destinationType:navigator.camera.DestinationType.DATA_URL,
-			 sourceType:navigator.camera.PictureSourceType.PHOTOLIBRARY});
-}
+      // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
 
-// Called if something bad happens.
-//
-function onFail(message) {
-    alert('Failed because: ' + message);
-}
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The in-line CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI
+      // console.log(imageURI);
+
+      // Get image handle
+      //
+      var largeImage = document.getElementById('largeImage');
+
+      // Unhide image elements
+      //
+      largeImage.style.display = 'block';
+
+      // Show the captured photo
+      // The in-line CSS rules are used to resize the image
+      //
+      largeImage.src = imageURI;
+    }
+
+    // A button will call this function
+    //
+    function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+    }
+
+    // A button will call this function
+    //
+    function capturePhotoEdit() {
+      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+    }
+
+    // A button will call this function
+    //
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+    }
+
+    // Called if something bad happens.
+    //
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
 
 //  });
     $('#Post').bind("click", function(e) {
     var noteText = $("#QuestionText").val();
-    var parsefile = new Parse.File("mypic.jpg", { base64: imagedata });
+    var parseFile = new Parse.File("mypic.jpg", {
+        base64: imagedata
+    });
     console.log(parseFile);
     parseFile.save().then(function() {
         var note = new Parse.Object.extend("Upload");
@@ -42,6 +101,7 @@ function onFail(message) {
         note.set("Photo", parseFile);
         note.save();
         alert("Success! You make check your results in your profile page");
-    });
+        location.reload();
 
+    });
 });
